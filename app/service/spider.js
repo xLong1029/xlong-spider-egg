2ff633523f4a9d101617e74524f33e4d8ae65212
xLong <381612175@qq.com>
2019-09-17 10:53:03 +0800

    完成：网页截图和HTML转PDF功能

--- app/service/spider.js
+++ app/service/spider.js
@@ -9,6 +9,12 @@ const path = require('path');
 // 加密模块
 const crypto = require('crypto');
 
+
+// 跳转等待时间
+const timeout = 10000;
+// 文档加载完才跳转页面
+const waitUntil = 'domcontentloaded';
+
 class SpiderService extends Service {
     /**
      * 获取数据
@@ -33,21 +39,18 @@ class SpiderService extends Service {
                 res.code = 404;
                 res.msg = '网站格式不正确，请重新输入';
             } else {
-                res.code = 200;
-                res.msg = '请求成功';
-
                 switch (type) {
                     case 1:
-                        res.data = await this.getScreenshot(web);
+                        res = await this.getScreenshot(web);
                         break;
                     case 2:
-                        res.data = await this.getPDF(web);
+                        res = await this.getPDF(web);
                         break;
                     case 3:
-                        res.data = await this.getInfo(web);
+                        res = await this.getInfo(web);
                         break;
                     default:
-                        res.msg = 'type参数错误，请排查';
+                        res = { code: 404 , msg: 'type参数错误，请排查'};
                 }
 
             }
@@ -138,57 +141,94 @@ class SpiderService extends Service {
         else return null;
     }
 
+    // 页面滚动获取数据
+    async pageAutoScroll(page) {
+        return page.evaluate(() => {
+            return new Promise((resolve, reject) => {
+                // 限制最大高度
+                const MAX_HEIGHT = 100000;
+
+                let totalHeight = 0;
+                let distance = 100;    
+
+                let timer = setInterval(() => {
+                    let scrollHeight = document.body.scrollHeight;
+
+                    // 滚动到指定位置
+                    window.scrollBy(0, distance);
+
+                    totalHeight += distance;
+                    if (totalHeight > MAX_HEIGHT){
+                        clearInterval(timer);
+                        resolve({ code: 404100, msg: '高度超过 100000px ，无法获取数据' });
+                    }
+                    if (totalHeight >= scrollHeight) {
+                        clearInterval(timer);
+                        resolve({ code: 200, msg: '滚屏截取数据成功' });
+                    }
+                }, 100);
+            })
+        });
+    }
+
     // 获取屏幕截图
     async getScreenshot(web) {
         // 创建一个浏览器实例 Browser 对象
         const browser = await puppeteer.launch({
-            defaultViewport: {width: 1920, height: 1080},
-            ignoreHTTPSErrors: true,
+            defaultViewport: { width: 1920, height: 1080 }, // 默认视图大小
+            ignoreHTTPSErrors: true, // 使用忽略 HTTPS 错误
+            // 浏览器配置
+            args: [
+                '--no-sandbox',
+                '--disable-infobars ', // 不显示信息栏
+                '--window-size=1920,1080', // 窗体大小
+                '--lang=zh-CN',
+                '--disable-dev-shm-usage'
+            ],
         });
+
         // 创建页面 Page 对象
         const page = await browser.newPage();
         // 跳转到指定的页面
-        await page.goto(web);
-        
-        // 获取页面标题
-        const title = await page.title();
-        const fileName = new Date().getTime() + crypto.createHash('md5').update(title).digest('hex') + '.jpeg';
-        // 对页面进行截图并保存
-        const dir = await this.getStoreDir('screenshot');        
-        await page.screenshot({ path: `${dir}/${fileName}` });
+        await page.goto(web, { timeout, waitUntil}).catch(err => { console.log(err) });
 
-        // 关闭浏览器
-        await browser.close();
+        const res = await this.pageAutoScroll(page);
+        if(res.code == 200){
+            // 获取页面标题
+            const title = await page.title();
+            const fileName = `${new Date().getTime() + crypto.createHash('md5').update(title).digest('hex')}.jpg`;
+            // 对页面进行截图并保存
+            const dir = await this.getStoreDir('screenshot');        
+            await page.screenshot({ path: `${dir}/${fileName}`, fullPage: true });
 
-        return await this.getFile(fileName, dir);
+            // 关闭浏览器
+            await browser.close();
+
+            const data = await this.getFile(fileName, dir);
+            return { code: 200 , data, msg: '请求成功'};
+        }
+        else return res;
     }
 
     // 获取PDF
+    // PDF在Chrome浏览器下，需要无头浏览器设置，解决“PrintToPDF is not implemented”的问题
     async getPDF(web) {
-        // 创建一个浏览器实例 Browser 对象
-        const browser = await puppeteer.launch({
-            defaultViewport: {width: 1920, height: 1080},
-            ignoreHTTPSErrors: true,
-        });
-        // 创建页面 Page 对象
-        const page = await browser.newPage();
-        // 跳转到指定的页面
-        await page.goto(web);
-        
-        // 获取页面标题
-        const title = await page.title();
-        const fileName = new Date().getTime() + crypto.createHash('md5').update(title).digest('hex') + '.pdf';
-        // 对页面进行截图并保存
-        const dir = await this.getStoreDir('pdf');        
-        await page.pdf({ path: `${dir}/${fileName}` });
+        const browser = await puppeteer.launch();
 
-        // 关闭浏览器
+        const page = await browser.newPage();
+        await page.goto(web, { timeout, waitUntil: 'networkidle0' }).catch(err => { console.log(err) });
+
+        const title = await page.title();
+        const fileName = `${new Date().getTime() + crypto.createHash('md5').update(title).digest('hex')}.pdf`;
+
+        const dir = await this.getStoreDir('pdf');        
+        await page.pdf({ path: `${dir}/${fileName}`, printBackground: true, width: '1920' });
+
         await browser.close();
 
-        return await this.getFile(fileName, dir);  
+        const data = await this.getFile(fileName, dir);
+        return { code: 200 , data, msg: '请求成功'};        
     }
-
-
 
     // 获取信息
     async getInfo(web) {
