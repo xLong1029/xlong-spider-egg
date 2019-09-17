ec7344e70e6384cce8628cc671df0237bcd64016
xLong <381612175@qq.com>
2019-09-17 13:01:17 +0800

    添加：弹窗提示

--- app/public/home/js/common.js
+++ app/public/home/js/common.js
@@ -1,7 +1,26 @@
+/*
+ * 功能 : 通用方法
+ * 作者 : 罗永梅（381612175@qq.com）
+ * 日期 : 2019-09-17
+ * 版本 : version 1.0
+ */
+
 // 设置默认图片
 function defaultImg(){
     var img = event.srcElement;
-    console.log(img); 
     img.src = "/public/images/default.jpg"; 
     img.onerror = null
+}
+
+// 设置默认图片
+function showAlertMsg(type, msg){
+    var $alert = $('#alert');
+    
+    $alert.attr('class', `message message-show message--${type}`);
+    $alert.find('.message__content').text(msg);
+
+    setTimeout(function(){
+        $alert.removeClass(`message-show`);
+        $alert.find('.message__content').text('');
+    }, 1000);
 }
